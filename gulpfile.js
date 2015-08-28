1c28ff8a2ce7e317a3d6ba7021f43e6f7d6b0adb
czytelny <czytelny@gmail.com>
2015-08-28 21:13:00 +0200

    uglify task added to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
+var uglify = require('gulp-uglify');
 
+gulp.task('uglify', function() {
+  return gulp.src('vnerv.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('test', function (done) {
     return new Server({
